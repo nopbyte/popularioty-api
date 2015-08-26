957e5f456bca8e06875ffc84e1d9e66db8307cf9
nopbyte <juandabytes@gmail.com>
2015-08-26 14:13:59 +0200

    update modules to latest versions

---
+++ curl/popularioty-calls/internal_api/send_popularity.sh
@@ -0,0 +1,13 @@
+url=localhost:9080
+curl -XPOST  -H "Content-Type: application/json;charset=UTF-8"  -H "Authorization: IYao9AeJcaUzhLPB0P1B"   -d '[{
+   "src":{
+	"service_instance":"0c739e21-decbb7959-4010-b244-6217789"
+   },
+   "dest":{
+	"service_instance":"0c739e21-decbb7959-4010-b244-61237789"	
+   },
+   "date":1427807745889	
+}]' "$url/internal/application-popularity/"
+
+
+#similarly,  application-activity will be used for contracts later
